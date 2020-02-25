1409bdc60cf48df728a5b07a51921c18b106b595
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-25 08:45:27 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,5 +6,8 @@ class Song < ApplicationRecord
 
   validates :artist_name, presence: true
 
-
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
 end
