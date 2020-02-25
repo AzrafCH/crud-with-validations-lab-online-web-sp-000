76bbb835ce938c817bb155517e022c71516417f1
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-25 08:51:26 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,26 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
 
+    if @song.save 
+      redirect_to song_path(@song)
+    else 
+      render :new 
+    end 
+  end 
+    
+  def edit 
+    @song = Song.find(params[:id])
+  end 
+
+  def update 
+    @song = Song.find(params[:id])
+
+    if @song.update 
+      redirect_to song_path(@song)
+    else 
+      render :edit 
+    end 
+  end 
 
   private
   def song_params
