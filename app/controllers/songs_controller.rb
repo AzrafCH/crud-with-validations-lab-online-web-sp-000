afb7b7f43d15e7b7d8e2bd572b76e5949cb519cb
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-25 08:49:34 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,12 +12,12 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    @song = Song.new(show_params)
+
 
   private
-
   def show_params
     params.require(:show).permit(:title, :released, :release_year, :genre, :artist_name)
   end
-
-
 end
