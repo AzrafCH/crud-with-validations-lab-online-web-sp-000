fce08b61da2a808cf6a5a2238410fc8b598560e6
Azraf Chowdhury <achowdhur@ltu.edu>
2020-02-25 08:40:19 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-  validates :title, uniqueness: true
+  validates :title, uniqueness: { scope %i[release_year artist_name], message: 'cannot be repeated by the same artist in the same year'}
 
 
 end
